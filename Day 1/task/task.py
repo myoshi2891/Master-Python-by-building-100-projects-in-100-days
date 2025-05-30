dfdc4b502532b608ddd936bcc17d87cf148e1133
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-05-31 00:23:30 +0900

    Day1 Working with Variables in Python to Manage Data

---
+++ Day 1/task/task.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator.")
+city = input("Which city did you grow up in?\n")
+print(city)
+pet = input("What is your pet's name?\n")
+print("Your band name could be:" + pet + city)
