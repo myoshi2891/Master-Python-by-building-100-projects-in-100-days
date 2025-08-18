b38168c103b6d8681b656728f70fbd6c93ef568e
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-08-19 02:25:36 +0900

    Day 10 Functions with Outputs

--- Day 10/Docstrings/task.py
+++ Day 10/Docstrings/task.py
@@ -1,4 +1,8 @@
 def format_name(f_name, l_name):
+    """
+    Take a first and last name and return a formatted name
+    """
+
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
@@ -6,7 +10,14 @@ def format_name(f_name, l_name):
 
 formatted_name = format_name("AnGeLa", "YU")
 
+print(formatted_name)
 length = len(formatted_name)
 
-
-
+# def function_1(text):
+#     return text + text
+#
+# def function_2(text):
+#     return text.title()
+#
+# output = function_2(function_1("hello"))
+# print(output)
