f0afc6e8d1ca730af6e5cd2b16ff04458aee3c8a
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-08-21 15:48:14 +0900

    Day13 Debugging

--- Day 13/Use Print/task.py
+++ Day 13/Use Print/task.py
@@ -1,5 +1,29 @@
-word_per_page = 0
-pages = int(input("Number of pages: "))
-word_per_page == int(input("Number of words per page: "))
-total_words = pages * word_per_page
-print(total_words)
+# pages = int(input("Number of pages: "))
+# word_per_page = int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# print(total_words)
+# def is_leap(year):
+#     if year % 400 == 0:
+#         return True
+#     elif year % 100 == 0 and year % 4 == 0:
+#         return False
+#     elif year % 4 == 0:
+#         return True
+#     else:
+#         return False
+#
+# print(is_leap(2020))
+
+# Target is the number up to which we count
+def fizz_buzz(target):
+    for number in range(1, target + 1):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
+
+fizz_buzz(20)
